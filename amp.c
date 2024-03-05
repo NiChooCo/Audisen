3c0f0e2a3afa1048c52fe67500f7fde68630028c
Oonilink <nicolas.beunon@isen-ouest.yncrea.fr>
2024-03-05 09:54:09 +0100

    oui j'ai fait readAMP

--- amp.c
+++ amp.c
@@ -23,16 +23,23 @@ FILE* initAMP(char* filename){
 
 
 
-/*void readAMP(FILE* pf, char * song_filename){
-    while(fgets(song_filename, MAX_SONGNAME_SIZE, pf)!=NULL){
-        fgets(song_filename, MAX_SONGNAME_SIZE, pf);
-        printf("%s", song_filename);
-    }
-}*/
 void readAMP(FILE* pf, char * song_filename){
     fgets(song_filename, MAX_SONGNAME_SIZE, pf);
-    for (int i = 0; i < strlen(song_filename); ++i) {
+    int count = strlen(song_filename);
+    for (int i = 0; i < count; ++i) {
         song_filename[i] = (char)tolower((int)song_filename[i]);
+        if(!(isalpha((int) song_filename[i]))){
+            song_filename[i] = '_';
+            //printf("|%c|", song_filename[i-1]);
+            if(song_filename[i-1] == '_'){
+                for(int j = i; j < count; j++){
+                    song_filename[j] = song_filename[j+1];
+                    song_filename[j] = (char)tolower((int)song_filename[j]);
+                }
+                count -= 1;
+                i-=1;
+            }
+        }
     }
     printf("%s\n", song_filename);
 }
