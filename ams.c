6ddfe6a0742e90439302bf35ebe9ec99b4bfbbff
NiChooCo <nicolas.taillebois@isen-ouest.yncrea.fr>
2024-03-05 13:51:43 +0100

    readAMS milieu

--- ams.c
+++ ams.c
@@ -11,7 +11,7 @@ s_song readAMS(char* fileName){
 	FILE * songFile;
 	songFile = fopen(fileName, "r");
 	if(songFile==NULL){
-		printf("Erreur, fichier non trouvé");
+		printf("Erreur, fichier non trouvé\n");
 		mySong.title[0] = '\0';
 		for (int i = 0; i < MAX_NUMBER_TICKS; ++i) {
 			mySong.tickTab[i].accent = 0;
@@ -27,6 +27,11 @@ s_song readAMS(char* fileName){
 		char line[MAX_SIZE_LINE];
 		fgets(line , TMP_MAX, songFile);
 		mySong.tpm = atoi(line);
+		for (int i = 0; i < MAX_NUMBER_TICKS; ++i) {
+			for (int j = 0; j < MAX_SIZE_LINE; ++j) {
+
+			}
+		}
 	}
 	printf("%s", mySong.title);
 	printf("%d", mySong.tpm);
